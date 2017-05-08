0d8deba0fbfb4f67d022644d005dd741aee60f90
waynebanks <thestorerun@yahoo.com>
2017-05-08 14:23:38 -0700

    added views, app.js, schema, and package.json

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app =express();
+
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+
+const queries = require('./database/db')
+
+
+app.use(express.static(__dirname + '/public'));
+// set up template
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/home', (req, res) => {
+	console.log('checking in from home!')
+	res.render('home');
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('the server is now running on port: ' + port);
+});
+
+module.exports = app;
---
+++ database/schema.sql
@@ -0,0 +1,45 @@
+DROP DATABASE IF EXISTS mentor_app;
+
+CREATE DATABASE mentor_app;
+
+\c mentor_app
+
+
+DROP TABLE IF EXISTS mentors;
+CREATE TABLE Mentors(
+id SERIAL PRIMARY KEY,
+username VARCHAR(30) NOT NULL,
+education TEXT,
+career_path TEXT,
+f_name TEXT,
+l_name TEXT,
+image TEXT,
+location TEXT,
+mos TEXT,
+branch TEXT,
+bio TEXT,
+veteran BOOLEAN,
+personal_interest TEXT
+);
+
+CREATE TABLE Mentees (
+id SERIAL PRIMARY KEY,
+username VARCHAR(30) NOT NULL,
+education text,
+career_path text,
+f_name text,
+l_name text,
+image text,
+location text,
+mos text,
+branch text,
+bio text,
+personal_interest text,
+mentor INTEGER REFERENCES Mentors(id)
+);
+
+DROP TABLE IF EXISTS mentorship;
+CREATE TABLE mentorship(
+mentors_id INTEGER,
+mentees_id INTEGER
+);
